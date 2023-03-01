7db7efa3d43543b7fc34cef5f3556e6ba274367f
Krystle5183 <96323148+Krystle5183@users.noreply.github.com>
2023-03-01 14:06:21 -0600

    Create main.py

---
+++ week5/developClass/main.py
@@ -0,0 +1,13 @@
+class Data:
+    def __init__(self, num):
+        self.list = []
+        self.num = num
+
+
+    def strList(self):
+        self.list.append(self.num)
+        return self
+
+    def searchList(self):
+        index = list.index("test2")
+        return index
